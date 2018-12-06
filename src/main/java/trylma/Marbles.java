c717dcc6141d746b3403b7ebe2c2291bfbe813c3
macgors <macgors@gmail.com>
2018-12-06 13:06:29 +0100

    Board.java and Marbles.java added, board is being drawn and the is some logic behind this. I propose that we treat marbles and board like that: marbles make up the board, they are one thing, when we move a marble we acctualy switch it with marble that was acting as a free space ( now free-space marbles are GRAY), this will make life easier.

---
+++ src/main/java/trylma/Marbles.java
@@ -0,0 +1,11 @@
+package trylma;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+public class Marbles extends javafx.scene.shape.Circle {
+        public void setColor(Color color) {
+            this.setFill(color);
+        }
+    }
+
