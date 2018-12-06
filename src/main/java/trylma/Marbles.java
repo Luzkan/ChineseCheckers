9289d657cd770c534420d60097ddd9a55c593cc0
Luzkan <luzak20000@gmail.com>
2018-12-06 14:58:31 +0100

    Added: - logic for checking if move is possible - movement of marbles (by switching colors) - cases for those to be possible
    Need:
    - Actual Player color

--- src/main/java/trylma/Marbles.java
+++ src/main/java/trylma/Marbles.java
@@ -4,8 +4,21 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
 public class Marbles extends javafx.scene.shape.Circle {
+
+        public void setDefaultGrayColor(Color color) {
+            this.setFill(Color.GRAY);
+        }
+
+        public void setDefaultPlayerColor(Color color) {
+            this.setFill(Color.DARKGRAY);
+        }
+
         public void setColor(Color color) {
             this.setFill(color);
+        }
+
+        public void getColor(Color color) {
+            this.getColor(color);
         }
     //Check ID on Click - just for debugging
 
