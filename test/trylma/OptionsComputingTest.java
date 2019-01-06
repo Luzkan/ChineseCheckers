c8ee4c14bf8b2c75401e0bfdf981876ff4484980
Luzkan <luzak20000@gmail.com>
2019-01-06 23:45:35 +0100

    1.14 Rule Update about Skipping Turn

--- test/trylma/OptionsComputingTest.java
+++ test/trylma/OptionsComputingTest.java
@@ -27,34 +27,4 @@ class OptionsComputingTest {
         int result = test.getNumberOfCPU();
         assertEquals(0, result);
     }
-
-    /*
-    (J) These test fails for some reason
-
-    @Test
-    void defaultValueTypeOfBoard(){
-        OptionsComputing test = new OptionsComputing();
-        //test.setTypeOfBoard("6-Shaped Start");
-        String result = test.getTypeOfBoard();
-        assertEquals("6-Shaped Start", result);
-    }
-
-    @Test
-    void defaultValueIP(){
-        OptionsComputing test = new OptionsComputing();
-        //test.setIPPort("localhost");
-        String result = test.getIPPort();
-        assertEquals("localhost", result);
-    }
-
-
-    @Test
-    void defaultValue(){
-        OptionsComputing test = new OptionsComputing();
-        //test.setIPPort("Blocked Switch");
-        String result = test.getRuleSet();
-        assertEquals("Blocked Switch", result);
-    }
-    */
-
 }
