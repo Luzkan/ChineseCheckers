1f3d583c10f71a1611eebfe240529fdb56ae1a92
macgors <macgors@gmail.com>
2019-01-07 00:31:49 +0100

    1.16 deleted usless methods

--- src/main/java/trylma/Marbles.java
+++ src/main/java/trylma/Marbles.java
@@ -12,39 +12,6 @@ public class Marbles extends javafx.scene.shape.Circle {
         this.setFill(Color.GRAY);
     }
 
-    public void setDefaultColorWherePlayerStarted(){
-        this.setFill(Color.DARKGRAY);
-    }
-
-    public void setPlayer1Color(){
-        this.setFill(Color.GREEN);
-    }
-
-    public void setPlayer2Color(){
-        this.setFill(Color.RED);
-    }
-
-    public void setPlayer3Color(){
-        this.setFill(Color.YELLOW);
-    }
-
-    public void setPlayer4Color(){
-        this.setFill(Color.BLUE);
-    }
-
-    public void setPlayer5Color(){
-        this.setFill(Color.PINK);
-    }
-
-    public void setPlayer6Color(){
-        this.setFill(Color.DARKMAGENTA);
-    }
-
-    /*
-    public void getColor(Color color) {
-        this.getColor(color);
-    }
-    I dont know why get Color should be null so i put it in comments couse it makes my life hard
-    */
+   
 }
 
